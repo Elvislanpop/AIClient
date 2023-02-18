178a6e513bb6296ab844e6ddbd2c135217491c2f
hyd <>
2023-02-18 18:33:01 +0800

    no message

--- AiClient/AiClient/ViewController.swift
+++ AiClient/AiClient/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
-
+import SnapKit
+import OpenAISwift
 class ViewController: UIViewController {
+    let openAI = OpenAISwift(authToken: "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")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
 
+        openAI.sendCompletion(with: "Swift网络请求", model: .gpt3(.davinci)) { result in // Result<OpenAI, OpenAIError>
+            switch result {
+            case .success(let success):
+                print(success.choices.first?.text ?? "")
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+
+//        let demoLab = TypewriterLabel()
+//        demoLab.textColor = .black
+//        demoLab.textAlignment = .center
+//        demoLab.font = .systemFont(ofSize: 16)
+//        self.view.addSubview(demoLab)
+//        demoLab.snp.makeConstraints { make in
+//            make.center.equalToSuperview()
+//            make.width.equalTo(375)
+//            make.height.equalTo(300)
+//        }
+    }
+    
+    
 
 }
 
